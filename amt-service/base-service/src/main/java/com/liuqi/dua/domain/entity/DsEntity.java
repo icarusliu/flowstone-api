e9db24154534b9d086463fd10e388072284cd55b
花石 <icarusliu@163.com>
2024-08-18 14:48:25 +0800

    目录名称调整

--- amt-service/base-service/src/main/java/com/liuqi/dua/domain/entity/DsEntity.java
+++ amt-service/base-service/src/main/java/com/liuqi/dua/domain/entity/DsEntity.java
@@ -15,10 +15,16 @@ import lombok.Data;
 @Comment("数据源配置")
 @TableName("d_ds")
 public class DsEntity extends BaseEntity {
+    @Comment("编码")
     private String code;
+    @Comment("名称")
     private String name;
+    @Comment("分类")
     private String type;
+    @Comment("地址")
     private String url;
+    @Comment("用户名")
     private String username;
+    @Comment("密码")
     private String password;
 }
