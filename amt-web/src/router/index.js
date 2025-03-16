d6bd75eec4e3dfdc7557938471fe8429e803aaa4
icaursliu <icarusliu@163.com>
2025-03-16 20:06:07 +0800

    接口分类功能移除，直接在树中进行编辑

--- amt-web/src/router/index.js
+++ amt-web/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from '../views/index.vue';
 import ApiIndex from '../views/api/index.vue';
-import Type from '../views/api/type.vue';
 import Ds from '../views/base/ds.vue';
 import LogIndex from '../views/api/logs/Index.vue';
 
